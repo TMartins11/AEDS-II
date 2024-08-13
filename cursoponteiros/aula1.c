04e64e796754b32fe1c08af99fc7cbda275b7c3c
Thiago Martins <thiago.martins1112@gmail.com>
2024-08-12 21:06:51 -0300

    Variáveis X Variáveis Ponteiro - FINALIZADA

--- cursoponteiros/aula1.c
+++ cursoponteiros/aula1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// È importante definir que & colocado antes de uma variável se refere ao endereço dela, e não ao conteúdo
+// È importante definir que & colocado antes de uma variável se refere ao endereço dela, e não ao conteúdo.
 
 int main(){
     // Variáveis X Váriaveis Ponteiro
@@ -10,31 +10,33 @@ int main(){
 
     printf("Considerando num1 = 10 e *pont1 = &num1\n");
 
-    // Imprime o valor armazenado na variável num1
+    // Imprime o valor armazenado na variável num1.
     printf("\nValor de num1: ");
     printf("%d\n", num1);
 
-    // Imprime o endereço da variável num1, o espaço na memória onde está armazenada
+    // Imprime o endereço da variável num1, o espaço na memória onde está armazenada.
     printf("Endereço de num1: ");
     printf("%p\n", &num1);
 
-    // Imprime o conteúdo de pont1 == endereço de num1, visto que trata-se de uma variável ponteiro
+    // Imprime o conteúdo de pont1 == endereço de num1, visto que trata-se de uma variável ponteiro.
     printf("\nConteúdo de pont1: ");
     printf("%p\n", pont1);
 
-    // Imprime o endereço da variável ponteiro pont1, visto que & denota endereço
+    // Imprime o endereço da variável ponteiro pont1, visto que & denota endereço.
     printf("Endereço de pont1: ");
     printf("%p\n", &pont1);
 
-    // Podemos observar que qualquer alteração ao valor de num1 não acarreta nenhuma mudança a seu endereço
+    // Podemos observar que qualquer alteração ao valor de num1 não acarreta nenhuma mudança a seu endereço.
     num1 = 50;
     printf("\nConsiderando agora num1 = 50 e *pont1 = &num1\n");
     printf("\nValor de num1: ");
+    printf("%d\n", num1);
+    printf("Endereço de num1: ");
     printf("%p\n", &num1);
     printf("\nConteúdo de pont1: ");
     printf("%p\n", pont1);
 
-    // Assim como, nenhuma alteração ao endereço que pont1 armazena altera o endereço do ponteiro que é pont1
+    // Assim como, nenhuma alteração ao endereço que pont1 armazena altera o endereço do ponteiro que é pont1.
     int num2 = 77;
     pont1 = &num2;
     printf("\nPara finalizar, consideramos num2 = 77 e pont1 = &num2\n");
