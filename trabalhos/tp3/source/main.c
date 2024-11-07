3b9abd4cac6a0c542f0df34d1d20fee2aa9d4a69
Thiago Martins <thiagomartins.silva@sou.unifal-mg.edu.br>
2024-11-06 21:04:54 -0300

    Base pro TP3

---
+++ trabalhos/tp3/source/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct jogadores{
+    char nome[50];
+    char posição[50];
+    char naturalidade[50];
+    char clube[50];
+    int idade;
+};
+
+int main(char argc, char *argv[]){
+
+    return 0;
+}
