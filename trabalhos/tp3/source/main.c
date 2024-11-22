d46391326173afd62c0fa16fb4f1a39be9d89d69
Thiago Martins <thiagomartins.silva@sou.unifal-mg.edu.br>
2024-11-22 19:09:36 -0300

    Leitura Implementada

--- trabalhos/tp3/source/main.c
+++ trabalhos/tp3/source/main.c
@@ -6,20 +6,16 @@
 
 typedef struct{
     char nome[MAX_STRING];
-    char posição[MAX_STRING];
+    char posicao[MAX_STRING];
     char naturalidade[MAX_STRING];
     char clube[MAX_STRING];
     int idade;
 }Jogador;
 
 int main(int argc, char* argv[]){
-    // Conferindo argumentos.
+    // Conferindo se a quantidade de argumentos é válida.
     if(argc != 2){
         printf("\nA quantidade de argumentos é inválida!");
-        printf("\nVocê deve inserir o algoritmo de ordenação que deseja utilizar: ");
-        printf("\n1 - BubbleSort - Ordenação Simples.");
-        printf("\n2 - MergeSort - Ordenação Ótima.");
-        printf("\n3 - Counting Sort - Ordenação em Tempo Linear.\n");
         return 1;
     }
         
@@ -36,13 +32,12 @@ int main(int argc, char* argv[]){
             break;
         default:
             printf("\nVocê escolheu um algoritmo inválido!\n");
+            printf("\nVocê deve escolher um dos algoritmos abaixo:");
+            printf("\n1 - BubbleSort - Ordenação Simples.");
+            printf("\n2 - MergeSort - Ordenação Ótima.");
+            printf("\n3 - Counting Sort - Ordenação em Tempo Linear.\n");
             return 1;
     }    
-
-    // Ponteiro para a struct que será utilizada, faremos alocação dinâmica de memória para preencher o vetor com as leituras do arquivo.
-    Jogador *jogadores = NULL;
-    int njog = 0;
-    int total = 0;
     
     // Abrindo o arquivo e checando se foi aberto corretamente.
     FILE *arq = fopen("jogadores.csv", "r");
@@ -51,45 +46,25 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    // Leitura dinâmica do arquivo.
-    while(1){
-        if(njog >= total){
-            total = total == 0 ? 10 : total * 2; // Se njog >= capacidade total, dobra a capacidade total e realoca a memória no ponteiro.
-            Jogador *tmp  = realloc(jogadores, total *sizeof(Jogador));
-            if(!tmp){
-                printf("\nErro de Alocação!");
-                free(jogadores);
-                fclose(arq);
-                return 1;
-            }
-            jogadores = tmp;
-        }    
-            if(fscanf(arq, "%49[^;];%49[^;];%49[^;];%49[^;];%d\n",
-                            jogadores[njog].nome,
-                            jogadores[njog].posição,
-                            jogadores[njog].naturalidade,
-                            jogadores[njog].clube,
-                            &jogadores[njog].idade) != 5){
-                break;                    
-            }
-            njog++;
+    // Variáveis para realizar a leitura e armazenar os dados na struct.
+    Jogador jogadores[1149];
+    int i = 0;
+    char linha[256]; // Buffer para armazenar as linhas lidas do arquivo.
+
+    fgets(linha, 256, arq); // Lendo e ignorando a primeira linha do arquivo (cabeçalho).
+
+    // Lendo o arquivo e armazenando os dados dos jogadores em um vetor.
+    while(fgets(linha,256,arq)){
+        if(sscanf(linha, "%[^,],%[^,],%[^,],%[^,],%d", jogadores[i].nome, jogadores[i].posicao, jogadores[i].naturalidade, jogadores[i].clube, &jogadores[i].idade) == 5){
+            i++; // Incrementando o índice do vetor.
+        }else{
+            break; // Quebra o looping caso não seja possível ler uma linha ou o arquivo tenha acabado.
+        }
     }
     
     // Fechando o arquivo após leitura.
     fclose(arq);
 
-    for(int i = 0; i < njog; i++){
-        printf("Jogador %d: %s | %s | %s | %s | %d\n", 
-               i+1, 
-               jogadores[i].nome, 
-               jogadores[i].posição, 
-               jogadores[i].naturalidade, 
-               jogadores[i].clube,
-               jogadores[i].idade);
-    }
-    
-    printf("%d", njog);
-    free(jogadores);
 
     return 0;
 }
